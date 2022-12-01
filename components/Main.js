f273992c33b35b0ce903a89c345bb54f9d4e80ee
Marshall Burns <marshallbcodes@gmail.com>
2022-12-01 11:50:00 -0600

    Squash bugs/stable in browser

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,7 @@
 import html from "html-literal";
-
 import * as views from "./views";
-export default state => html`
-  ${views[state.views](state)}
-`;
 
-//the export above is importing all the objects from "./countries"
+export default state => html`
+  ${console.table(views)}
+  ${views[state.view](state)}
+`;
