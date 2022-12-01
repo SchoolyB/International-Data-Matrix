c0377a3dc6a8717a33d6a969ec139816f5478cea
Marshall Burns <marshallbcodes@gmail.com>
2022-11-30 19:51:44 -0600

    Minor changes/add modules

---
+++ components/countries/index.js
@@ -0,0 +1,4 @@
+export { default as Belarus } from "./Belarus";
+export { default as Japan } from "./Japan";
+export { default as Morocco } from "./Morocco";
+export { default as Usa } from "./Usa";
