add64e84fddf7a282607164d25270f47b7148be4
Marshall Burns <marshallbcodes@gmail.com>
2022-12-08 17:58:04 -0600

    Update contact page/update CSS/update TODO.txt

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,15 +1,16 @@
 import html from "html-literal";
 export default () => html`
   <div id="contactContainer">
+    <h1>Contact Me</h1>
     <form action="https://formspree.io/f/mknelldn" method="POST">
-      <label>
-        <input type="text" name = "name" placeholder="Enter your name"></input>
+      <label  class="contactLabel"id="contactName">
+        <input class="contactInput" type="text" name = "name" placeholder="Enter your name"></input>
       </label>
-      <label>
-        <input type="email" name="email" placeholder="Enter your email" />
+      <label class="contactLabel"id="contactEmail">
+        <input  class="contactInput" type="email" name="email" placeholder="Enter your email" />
       </label>
-      <label>
-        <textarea name="message" placeholder="Enter your message"></textarea>
+      <label class="contactLabel" id="contactMsg">
+        <textarea id="contactTextArea" rows="16" cols="35" name="message" placeholder="Enter your message"></textarea>
       </label>
       <button  id="contactSubmitBtn" type="submit">Send</button>
     </form>
--- store/Contact.js
+++ store/Contact.js
@@ -1,4 +1,4 @@
 export default {
-  header: "Contact Me",
+  header: "Get In Touch",
   view: "Contact"
 };
