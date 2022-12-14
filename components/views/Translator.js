d0f80553eb7d08ab9c58d7353e046442c06ee9dd
Marshall Burns <marshallbcodes@gmail.com>
2022-12-14 17:48:04 -0600

    Update translator page/minor CSS changes/update TODO.txt

--- components/views/Translator.js
+++ components/views/Translator.js
@@ -1,9 +1,46 @@
 import html from "html-literal";
-
+import btn from "../../assets/images/translateBtn.png";
 export default () => html`
-  <div id="translatorContainer">
-    <div id="translateInput">INPUT</div>
-    <div id="translateBtn"><img src="$1" /></div>
-    <div id="translateOutput">OUTPUT</div>
+  <div id="fullTranslateContainer">
+    <div id="translatorContainer">
+      <div id="inputSide">
+        <select class="langSelect" id="inputLangSelect">
+          <option>Arabic</option>
+          <option>Belarusian</option>
+          <option>English</option>
+          <option>Greek</option>
+          <option>Japanese</option>
+          <option>Nepali</option>
+          <option>Russian</option>
+          <option>Spanish</option>
+        </select>
+        <div>
+          <textarea
+            id="translateInput"
+            placeholder="Please enter a word or phrase to translate"
+          ></textarea>
+        </div>
+      </div>
+
+      <div>
+        <img id="translateBtn" src=${btn} />
+      </div>
+
+      <div id="outputSide">
+        <select class="langSelect" id="outputLangSelect">
+          <option>Arabic</option>
+          <option>Belarusian</option>
+          <option>English</option>
+          <option>Greek</option>
+          <option>Japanese</option>
+          <option>Nepali</option>
+          <option>Russian</option>
+          <option>Spanish</option>
+        </select>
+        <div>
+          <textarea id="translateOutput" placeholder="Translation"></textarea>
+        </div>
+      </div>
+    </div>
   </div>
 `;
