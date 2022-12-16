a53da977b765d1e67ffad9ea0c18f5121bff9765
Marshall Burns <marshallbcodes@gmail.com>
2022-12-16 17:10:53 -0600

    Fix bugs/update package.json/remove unused code

--- index.js
+++ index.js
@@ -2,19 +2,54 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store"; //importing everything from "./store" as an object called "store"
 import Navigo from "navigo";
 import { capitalize } from "lodash";
-import dotenv from "dotenv";
+import axios from "axios";
 import * as deepl from "deepl-node";
 
-dotenv.config();
 const router = new Navigo("/");
 
 function render(state = store.Home) {
-  console.table(`${state}`);
   document.querySelector("#root").innerHTML = `
 
   ${Header(state)}
   ${Main(state)}
   ${Footer(state)}
   `;
+  afterRender(state);
   router.updatePageLinks();
 }
+
+const afterRender = state => {
+  //  add menu toggle to bars icon in nav bar
+  // document.querySelector(".fa-bars").addEventListener("click", () => {
+  //   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  // });
+    document.getElementById("translateBtn").addEventListener("click", event => {
+      console.log(event);
+
+      const authKey = `${process.env.TRANSLATION_KEY}`;
+
+router.hooks({
+  before: (done, params) => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    switch (view) {
+      // case "Translate":
+      //   // New Axios get request utilizing already made environment variable
+      //         //   break;
+      default:
+        done();
+    }
+  }
+});
+
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    }
+  })
+  .resolve(); //similar to "listen" method in expressJS
