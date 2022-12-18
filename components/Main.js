9531cfd6aca39c08973954b4f0a79c5b9de2e80b
Marshall Burns <marshallbcodes@gmail.com>
2022-12-18 16:44:51 -0600

    Minor changes/add comments

--- components/Main.js
+++ components/Main.js
@@ -3,4 +3,10 @@ import * as views from "./views";
 
 export default state => html`
   ${views[state.view](state)}
+  <!-- this function foes toto whatever view we are in,
+  passes in state / renders the view file-->
 `;
+
+// state is s chunk of data specific to a view or component
+
+// store is a collection of states
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store"; //importing everything from "./store" as an object called "store"
-import Navigo from "navigo";
+import Navigo from "navigo"; //the navigo library is what imports the router.hooks({}):before, router.on(), and .resolve()
 import { capitalize } from "lodash";
 import axios from "axios";
 import * as deepl from "deepl-node";
 
-const router = new Navigo("/");
+const router = new Navigo("/"); //initiating the "router" variable. uses the navigo library
 
+// declaring what the render() will do
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
 
@@ -14,34 +15,26 @@ function render(state = store.Home) {
   ${Main(state)}
   ${Footer(state)}
   `;
-
-  router.updatePageLinks();
+  afterRender(state);
+  router.updatePageLinks(); //navigo stuff for links
 }
 
-const authKey = `${process.env.TRANSLATION_KEY}`;
+function afterRender(state) {}
 
-router.hooks({
-  before: (done, params) => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
-    switch (view) {
-      // case "Translate":
-      //   // New Axios get request utilizing already made environment variable
-      //         //   break;
-      default:
-        done();
-    }
-  }
-});
+// router hooks({}) need a switch case statement to tell the program to specify what data should appear on what particular view
+// router.hooks({}) :before does all the data collection before the render method is executed
+// thats why our APIs usually are called within the function
+// router hooks runs until thew done() function is executed
 
-router
+router //this renders a specific view
   .on({
-    "/": () => render(),
+    "/": () => render(), //telling me that if the URL has no "/" then render the home page
     ":view": params => {
+      //telling me that if any views are inside the URL then capitalize the first letter then render the view
       let view = capitalize(params.data.view);
       render(store[view]);
     }
   })
-  .resolve(); //similar to "listen" method in expressJS
+  .resolve(); //similar to "listen" method in expressJS.
+// essentially says "ok I want to render the page, but first I need to run any 'router.hooks({}) :before' if there are any
+// then I can run the render function"
