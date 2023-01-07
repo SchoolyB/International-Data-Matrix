43ee5c2e1ddc6624cf1eee155610bac1c479a267
Marshall Burns <marshallbcodes@gmail.com>
2023-01-07 10:04:34 -0600

    Clean up some styling/add an asset

--- components/Home.js
+++ components/Home.js
@@ -4,13 +4,14 @@ export default () => html`
   <div id="welcomeContainer">
     <h1 id="welcomeMainHeading">The Entire World All In One Place</h1>
     <p id="welcomeParagraph">
-      The International Data Matrix or "TIDM" acts as a central place on the
-      internet that anyone can access to gain<br />
-      historical, cultural, religious, geographical, geo-economical and
-      geo-political understanding about any country<br />
-      in the world and the people that reside in them. To get started click on
-      the button below or search for a country<br />in the search field located
-      in the top right corner of your screen.
+      The International Data Matrix or The "IDM" acts as a central place on the
+      internet that anyone can access to gain historical,<br />
+      cultural, religious, geographical, geo-economical and geo-political
+      understanding about any country in the world and the<br />
+      people that reside in them. To get started you can click on the button
+      below to view a list of countries, click the logo in the <br />
+      top left corner to open the nav menu, or search for a country in the
+      search field located in the top right corner.
     </p>
     <div id="welcomeBtnContainer">
       <a href="Countries" role="button" id="welcomeBtn">View Countries</a>
