045f1b876fc25c9311a63225a7f7b76a31377a68
Marshall Burns <marshallbcodes@gmail.com>
2023-02-12 19:36:01 -0600

    Start country selection page rebuild

--- client/src/pages/Countries.tsx
+++ client/src/pages/Countries.tsx
@@ -1,66 +1,64 @@
 import axios from 'axios'
+import { count } from 'console';
 import React, { useCallback, useEffect, useState } from 'react'
 import { api } from '../includes/api'
 import { CountryListData } from '../types/countryListData'
-// import * as flags from "../../assets/countrySelctionFlags";
+// import * as flags from "../../assets/countrySelectionFlags";
 
-
+const countryListDataFlags = import.meta.glob('../../assets/countryPageFlags/svgs/*.svg', {
+  eager: true
+});
 
 
 export default function Countries() {
 
-
-
-const [countryList, setCountryList] = useState<CountryListData[]>([])
+// console.log(countryListDataFlags)
+  const [countryList, setCountryList] = useState<CountryListData[]>([])
 
 const fetchCountryList = useCallback(() => {
   return api.get('/Countries').then(res => {
-
-    console.log(res.data)
+    setCountryList(res.data)
   }).catch(error => {
         console.error(error)
     });
   },[])
   useEffect(() => {
   fetchCountryList()
-}, [fetchCountryList])
+  }, [fetchCountryList])
+  console.log(countryList)
 
 
+  const countryName = countryList.map((country) => {
+    return <div>{country.name}</div>
+  })
+
+  const countryGDP = countryList.map((country) => {
+    return <p>{country.gdp}</p>
+  })
+
+  const countryLandMass = countryList.map((country) => {
+  return <p>{country.landMass}</p>
+  })
+
+  const countryFlag = countryList.map((country) => {
+    return <a href={country.link}><img className='flag'
+    src={country.flag} alt={country.flagAlt} /></a>
+  })
 
   return (
-    <div>
-
-  <div id="container">
-    {/* {state.filteredData
-      .map(country => {
-          <div id="overAllContainer">
-            <div className="country" id="{country._id}">
-              <h3 className="countryHeading">{country.name}</h3>
-              <a href="{country.link}">
-                <img
-                  className="flag"
-                  src={flags[country.name]}
-                  alt={country.flagAlt}
-                />
-              </a>
-              <div className="data">
-                <p>{country.population}</p>
-                <p>{country.gdp}</p>
-                <p>{country.landMass}</p>
-                <p></p>
-              </div>
+    <>
+      <div id='container'>
+        <div id='overAllContainer'>
+          <div className='country'>
+            <h3 className='countryHeading'>{countryName}</h3>
+            {countryFlag}
+            <div className="countrySubHeading">
+                {countryGDP}
+                {countryLandMass}
             </div>
           </div>
-      })
-      .join("")} */}
-
-
-
-  </div>
-
-
-
-
-    </div>
+        </div>
+      </div>
+    </>
   )
 }
