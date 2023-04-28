cfcb54cd6e9d2a1b2978199ae23e151d3441b162
Monique <skaiwilliams85@gmail.com>
2023-04-27 22:27:39 -0500

    fixed footer

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Container from "../components/helpers/Container";
 
 const getCountry = () => {
   axios.get("https://restcountries.com/v2/alpha/by").then((res) => {});
@@ -8,20 +9,21 @@ getCountry();
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <div id="welcomeContainer">
         <h1 id="welcomeMainHeading">The Entire World All In One Place</h1>
         <p id="welcomeParagraph">
           The International Data Matrix or The "IDM" acts as a central place on
-          the internet that anyone can access to gain historical,
-          <br />
-          cultural, religious, geographical, geo-economical and geo-political
-          understanding about any country in the world and the
-          <br />
-          people that reside in them. To learn more about a country you can
-          click on the button below or click the "Country Selection"
-          <br />
-          tab in the nav menu above. Welcome To The International Data Matrix.
+          the internet that anyone can access to gain historical, cultural,
+          religious, geographical, geo-economical and geo-political
+          understanding about any country in the world and the people that
+          reside in them.
+        </p>
+
+        <p id="welcomeParagraph">
+          To learn more about a country you can click on the button below or
+          click the "Country Selection" tab in the nav menu above. Welcome To
+          The International Data Matrix.
         </p>
         <div id="welcomeBtnContainer">
           <a href="Countries" role="button" id="welcomeBtn">
@@ -29,6 +31,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
