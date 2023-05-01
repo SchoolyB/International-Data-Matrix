688f4a120a67c36b95be7a900c06fbb57b0dbbc2
Marshall Burns <marshallbcodes@gmail.com>
2023-05-01 11:12:32 -0500

    Rebuild globe/add client lib folder/minor changes

--- client/src/pages/globe.tsx
+++ client/src/pages/globe.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import SceneInitializer from '../lib/SceneInitializer.js'
 
 export default function globe() {
-	return <div>{/* STUFF GOES HERE */}</div>
+	useEffect(() => {
+		const spoolUp = new SceneInitializer('globe')
+		spoolUp.initialize()
+		spoolUp.animate()
+
+		const boxGeometry = new THREE.BoxGeometry(16, 16, 16)
+		const boxMaterial = new THREE.MeshNormalMaterial()
+		const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
+
+		spoolUp.scene.add(boxMesh)
+	}, [])
+
+	return (
+		<>
+			<canvas id="globe"></canvas>
+		</>
+	)
 }
