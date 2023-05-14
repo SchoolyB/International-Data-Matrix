ccf70199f8f962e15c4606192c689facb908a59c
Marshall Burns <marshallbcodes@gmail.com>
2023-05-14 10:47:56 -0500

    Update country data axios to https

--- client/src/includes/countries.ts
+++ client/src/includes/countries.ts
@@ -3,6 +3,6 @@ import { CountryData } from '../types/countryData'
 
 export function getCountry(id: string) {
 	return axios
-		.get<CountryData>(`http://schoolyb.github.io/v1/data/country/${id}.json`)
+		.get<CountryData>(`https://schoolyb.github.io/v1/data/country/${id}.json`)
 		.then((res) => res.data)
 }
