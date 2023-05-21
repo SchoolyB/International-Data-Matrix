816ca23ad9b16907df5f57ed2c2a3964ac1f93fc
Michael Hinterdorfer <20180021@students.htl-perg.ac.at>
2023-05-21 17:40:14 +0200

    added link to map page to header as well as footer #224

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,61 +5,67 @@ import translator from '../../assets/footerIcons/comments.svg'
 import React from 'react'
 
 export default function Footer() {
-	return (
-		<footer>
-			{/* author name */}
-			<ul id="webPageInfo">
-				<li>Author: Marshall Burns</li>
-			</ul>
-			{/*footer icons*/}
+    return (
+        <footer>
+            {/* author name */}
+            <ul id="webPageInfo">
+                <li>Author: Marshall Burns</li>
+            </ul>
+            {/*footer icons*/}
 
-			<ul id="footerIconContainer">
-				{/* home icon */}
-				<li>
-					<a title="Home" href="/">
-						<img title="Home" className="footerIcon" id="homeIcon" src={home} />
-					</a>
-				</li>
+            <ul id="footerIconContainer">
+                {/* home icon */}
+                <li>
+                    <a title="Home" href="/">
+                        <img title="Home" className="footerIcon" id="homeIcon" src={home} />
+                    </a>
+                </li>
 
-				{/* documentation icon */}
-				<li>
-					<a
-						title="Documentation"
-						href="https://github.com/SchoolyB/Capstone/blob/master/README.md"
-					>
-						<img
-							title="Documentation"
-							className="footerIcon"
-							id="docIcon"
-							src={documentation}
-						/>
-					</a>
-				</li>
+                {/* documentation icon */}
+                <li>
+                    <a
+                        title="Documentation"
+                        href="https://github.com/SchoolyB/Capstone/blob/master/README.md"
+                    >
+                        <img
+                            title="Documentation"
+                            className="footerIcon"
+                            id="docIcon"
+                            src={documentation}
+                        />
+                    </a>
+                </li>
 
-				{/* translator icon */}
-				<li>
-					<a title="Translator" href="Translator">
-						<img
-							title="Translator"
-							className="footerIcon"
-							id="translateIcon"
-							src={translator}
-						/>
-					</a>
-				</li>
+                {/* translator icon */}
+                <li>
+                    <a title="Translator" href="Translator">
+                        <img
+                            title="Translator"
+                            className="footerIcon"
+                            id="translateIcon"
+                            src={translator}
+                        />
+                    </a>
+                </li>
 
-				{/* contact icon */}
-				<li id="footerContactIcon">
-					<a title="Contact" href="Contact">
-						<img
-							title="Contact"
-							className="footerIcon"
-							id="ContactIcon"
-							src={contact}
-						/>
-					</a>
-				</li>
-			</ul>
-		</footer>
-	)
+                {/* contact icon */}
+                <li id="footerContactIcon">
+                    <a title="Contact" href="Contact">
+                        <img
+                            title="Contact"
+                            className="footerIcon"
+                            id="ContactIcon"
+                            src={contact}
+                        />
+                    </a>
+                </li>
+
+                <li>
+                    <a title="Map" href="Map">
+                        ---Map-Icon---
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,50 +1,50 @@
-import HomeIcon from '@rsuite/icons/legacy/Home'
 import { useContext, useState } from 'react'
 import { Nav, Navbar } from 'rsuite'
 import { SearchContext } from '../App'
 import Container from './helpers/Container'
+import MenuIcon from '@rsuite/icons/Menu'
 import { ChangeEventHandler } from 'react'
 
 export default function Header() {
-	const search = useContext(SearchContext)
+    const search = useContext(SearchContext)
 
-	const [openMenu, setOpenMenu] = useState(false)
+    const [openMenu, setOpenMenu] = useState(false)
 
-	const handleMenu = () => setOpenMenu((state) => !state)
+    const handleMenu = () => setOpenMenu((state) => !state)
 
-	return (
-		<header>
-			<Navbar id="navbar">
-				<Container>
-					<div id="navHead">
-						<Navbar.Brand
-							id="logoText"
-							href="/"
-						>
-							The International Data Matrix
-						</Navbar.Brand>
-						<div onClick={handleMenu}>
-							<HomeIcon />
-						</div>
-					</div>
-					{openMenu && (
-						<div id="menu">
-							<Nav id="nav">
-								<Nav.Item
-									id="item"
-									icon={<HomeIcon />}
-									href="/"
-								>
-									Home
-								</Nav.Item>
-								<Nav.Item href="Countries">Country Selection</Nav.Item>
-								<Nav.Item href="Translator">Translator</Nav.Item>
-								<Nav.Item href="Contact">Contact</Nav.Item>
-							</Nav>
-						</div>
-					)}
-				</Container>
-			</Navbar>
-		</header>
-	)
+    return (
+        <header>
+            <Navbar id="navbar">
+                <Container>
+                    <div id="navHead">
+                        <Navbar.Brand
+                            id="logoText"
+                            href="/"
+                        >
+                            The International Data Matrix
+                        </Navbar.Brand>
+                        <div onClick={handleMenu}>
+                            <MenuIcon />
+                        </div>
+                    </div>
+                    {openMenu && (
+                        <div id="menu">
+                            <Nav id="nav">
+                                <Nav.Item
+                                    id="item"
+                                    href="/"
+                                >
+                                    Home
+                                </Nav.Item>
+                                <Nav.Item href="Countries">Country Selection</Nav.Item>
+                                <Nav.Item href="Translator">Translator</Nav.Item>
+                                <Nav.Item href="Contact">Contact</Nav.Item>
+                                <Nav.Item href='Map'>Map</Nav.Item>
+                            </Nav>
+                        </div>
+                    )}
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
