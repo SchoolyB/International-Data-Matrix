d8c2bb487829caf90958e17b478384d1d89e82bf
Marshall Burns <marshallbcodes@gmail.com>
2023-05-22 09:51:17 -0500

    Start migration from express to fastify/install dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,38 +1,26 @@
-import express, { Express } from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import countries from './routes/countriesRoute'
-import translator from './routes/translateRoute'
+import fastify from "fastify";
+import cors from "@fastify/cors";
 
-dotenv.config() //allows server to read DB key from a .env file
 
-const PORT = process.env.PORT || 4042 // we use || to provide a default value
-const app = express()
+const app = fastify({
+  logger: false // set to true for production
+})
+app.register(cors, {
+  origin: 'http://localhost:5173' //for development
+  // options: 'https://international-data-matrix.vercel.app' //for production
+})
 
-mongoose.connect(`${process.env.DATA_BASE}`)
-
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'Connection Error:'))
-db.once(
-	'open',
-	console.log.bind(console, 'Successfully connected to database!'),
-)
-
-const logging = (request: any, response: any, next: any) => {
-	console.log(`${request.method} ${request.url}`)
-	next()
+const start = () => {
+  try {
+    app.listen({
+      port: 4042, host: ''
+    }, (err,address) => {
+      console.log(`Server listening on ${address}`)
+     })
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
 }
-const allowedOrigins = ['https://international-data-matrix.vercel.app']
-// const allowedOrigins = ['http://localhost:5173']
-const options: cors.CorsOptions = {
-	origin: allowedOrigins,
-}
-// CALLING ALL EXPRESS .use methods
-app.use(cors(options))
-app.use(express.json())
-app.use(logging)
-app.use('/Countries', countries)
-app.use('/translator', translator)
-//Listen for what port server is running on and log it.
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+start()
