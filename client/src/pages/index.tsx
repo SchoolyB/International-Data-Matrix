fdc11ad667006aa3218099522f8b6aa555e23b5d
Marshall Burns <marshallbcodes@gmail.com>
2023-05-26 08:03:20 -0500

    Clean homepage/start building FAQ

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,36 +1,33 @@
-import axios from "axios";
-import Container from "../components/helpers/Container";
+import axios from 'axios'
+import Container from '../components/helpers/Container'
 
 const getCountry = () => {
-  axios.get("https://restcountries.com/v2/alpha/by").then((res) => {});
-};
+  axios.get('https://restcountries.com/v2/alpha/by').then(res => {})
+}
 
-getCountry();
+getCountry()
 
 export default function Home() {
   return (
     <Container>
-      <div id="welcomeContainer">
-        <h1 id="welcomeMainHeading">The Entire World All In One Place</h1>
-        <p id="welcomeParagraph">
-          The International Data Matrix or The "IDM" acts as a central place on
-          the internet that anyone can access to gain historical, cultural,
-          religious, geographical, geo-economical and geo-political
-          understanding about any country in the world and the people that
-          reside in them.
+      <div id='welcomeContainer'>
+        <h1 id='welcomeMainHeading'>The Entire World All In One Place</h1>
+        <p id='welcomeParagraph'>
+          The International Data Matrix is a resource that anyone can use to
+          learn about a country's historical, cultural, religious, geographical,
+          geo-economical and geo-political information.
         </p>
 
-        <p id="welcomeParagraph">
+        <p id='welcomeParagraph'>
           To learn more about a country you can click on the button below or
-          click the "Country Selection" tab in the nav menu above. Welcome To
-          The International Data Matrix.
+          click on "Country Selection" in the nav menu above.
         </p>
-        <div id="welcomeBtnContainer">
-          <a href="Countries" role="button" id="welcomeBtn">
+        <div id='welcomeBtnContainer'>
+          <a href='Countries' role='button' id='welcomeBtn'>
             View Countries
           </a>
         </div>
       </div>
     </Container>
-  );
+  )
 }
