d482bab0c060e23fb2d74c1e6710e4d3559131c5
Marshall Burns <marshallbcodes@gmail.com>
2023-05-26 21:33:34 -0500

    Update FAQ/add FAQ link to nav menu

--- client/src/pages/Faq.tsx
+++ client/src/pages/Faq.tsx
@@ -9,37 +9,40 @@ export default function FAQ() {
           {/* start of FAQ top row */}
 
           <div id='faqTopRow' className='faqRow'>
+            {/* 1st FAQ */}
             <div id='faq1' className='faqQuestion'>
-              <h2 className='faqQuestionHeading'>
-                What is the International Data Matrix?
-              </h2>
+              <h3 className='faqQuestionHeading'>
+                What Is The International Data Matrix?
+              </h3>
               <p className='faqQuestionParagraph'>
                 The International Data Matrix is a resource that anyone can use
                 to learn about a country's historical, cultural, religious,
                 geographical, geo-economical and geo-political information.
               </p>
             </div>
-
+            {/* 2nd FAQ */}
             <div id='faq2' className='faqQuestion'>
-              <h2 className='faqQuestionHeading'>
-                What can I do with International Data Matrix do?
-              </h2>
+              <h3 className='faqQuestionHeading'>
+                What Can The International Data Matrix Do?
+              </h3>
               <p className='faqQuestionParagraph'>
                 The International Data Matrix has several features that allow
                 users gain more knowledge and understanding about a country.
                 Currently those features include:
+                <br />
+                <br />
                 <ul>
-                  <li>an interactive map page</li>
-                  <li>a country selection page</li>
-                  <li>a translator page</li>
-                  <li>individual country informational pages</li>
+                  <li>an interactive map</li>
+                  <li>a translator</li>
+                  <li>country informational pages</li>
                 </ul>
               </p>
             </div>
+            {/* 3rd FAQ */}
             <div id='faq3' className='faqQuestion'>
-              <h2 className='faqQuestionHeading'>
-                Can I translate any language?
-              </h2>
+              <h3 className='faqQuestionHeading'>
+                How Many Languages Does The Translator Have?
+              </h3>
               <p className='faqQuestionParagraph'>
                 The translator can translate any language that is supported by
                 the DeepL translate API. Currently the DeepL Translate API
@@ -51,10 +54,11 @@ export default function FAQ() {
           {/* start FAQ bottom row */}
 
           <div id='faqBottomRow' className='faqRow'>
+            {/* 4th FAQ */}
             <div id='faq4' className='faqQuestion'>
-              <h2 className='faqQuestionHeading'>
-                What is the country selection page?
-              </h2>
+              <h3 className='faqQuestionHeading'>
+                What Is The Country Selection page?
+              </h3>
               <p className='faqQuestionParagraph'>
                 The country selection page is a page that allows users to select
                 a country and learn more about it. The country selection page
@@ -63,20 +67,20 @@ export default function FAQ() {
                 redirected to the country's informational page.
               </p>
             </div>
+            {/* 5th FAQ */}
             <div id='faq5' className='faqQuestion'>
-              <h2 className='faqQuestionHeading'>
-                What is the country informational page?
-              </h2>
+              <h3 className='faqQuestionHeading'>
+                What Is The Country Informational Page?
+              </h3>
               <p className='faqQuestionParagraph'>
                 The country informational page is a page that contains
                 historical, cultural, religious, geographical, economical and
                 political information about a country.
               </p>
             </div>
+            {/* 6th FAQ */}
             <div id='faq6' className='faqQuestion'>
-              <h2 className='faqQuestionHeading'>
-                How do I contribute to the International Data Matrix?
-              </h2>
+              <h3 className='faqQuestionHeading'>How Can I Contribute?</h3>
               <p className='faqQuestionParagraph'>
                 To contribute to the International Data Matrix you can start by
                 reading the CONTRIBUTING.md in the International Data Matrix
@@ -85,7 +89,7 @@ export default function FAQ() {
                   href='https://github.com/SchoolyB/International-Data-Matrix/blob/master/CONTRIBUTING.md'
                   target='_blank'
                 >
-                  <i>here</i>
+                  <i> here </i>
                 </a>
                 or by clicking the documentation icon in the footer of the
                 website.
