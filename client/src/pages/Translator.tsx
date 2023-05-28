81cce518c0d63e88688be690c0448bbc930fce32
Marshall Burns <marshallbcodes@gmail.com>
2023-05-28 10:29:23 -0500

    Add translator mobile responsiveness /fix styling - closes #223 #228

--- client/src/pages/Translator.tsx
+++ client/src/pages/Translator.tsx
@@ -21,77 +21,84 @@ export default function Translator() {
   }
 
   return (
-    <div>
-      <form id='fullTranslateContainer' onSubmit={handleTranslation}>
-        <div id='translateForm'>
-          <div id='inputSide'>
-            <div>
-              <textarea
-                id='translateInput'
-                placeholder='Please enter a word or phrase to translate'
-                value={sourceText}
-                onChange={event => setSourceText(event.target.value)}
-              />
+    <>
+      <div className='translatorContainer'>
+        <form className='translatorForm' onSubmit={handleTranslation}>
+          <div className='translatorBoxes'>
+            <div id='inputSide' className='boxContainer'>
+              <div>
+                <textarea
+                  className='translatorTextAreas'
+                  placeholder='Please enter a word or phrase to translate'
+                  value={sourceText}
+                  onChange={event => setSourceText(event.target.value)}
+                />
+              </div>
             </div>
-          </div>
+            <div className='languageSelectContainer'>
+              <div className='submission'>
+                <div>
+                  <button title='Submit' type='submit' className='translateBtn'>
+                    <img
+                      alt='A submit button'
+                      className='translateIcon'
+                      src='../../assets/translatorPageImages/translation.png'
+                    />
+                  </button>
+                </div>
+              </div>
 
-          <div>
-            <button title='Submit' type='submit' id='translateBtn'></button>
-          </div>
-
-          <div id='outputSide'>
-            <select
-              title='Language Select'
-              className='langSelect'
-              id='outputLangSelect'
-              value={outputLanguage}
-              onChange={event => setOutputLanguage(event.target.value)}
-            >
-              <option value='bg'>Bulgarian</option>
-              <option value='zh'>Chinese</option>
-              <option value='da'>Danish</option>
-              <option value='nl'>Dutch</option>
-              <option value='es-us'>English(American)</option>
-              <option value='en-gb'>English(British)</option>
-              <option value='et'>Estonian</option>
-              <option value='fi'>Finnish</option>
-              <option value='fr'>French</option>
-              <option value='de'>German</option>
-              <option value='el'>Greek</option>
-              <option value='hu'>Hungarian</option>
-              <option value='id'>Indonesian</option>
-              <option value='it'>Italian</option>
-              <option value='ja'>Japanese</option>
-              <option value='lt'>Lithuanian</option>
-              <option value='lv'>Latvian</option>
-              <option value='pl'>Polish</option>
-              <option value='pt-br'>Portuguese(Brazilian)</option>
-              <option value='pt-pt'>Portuguese(Portugal)</option>
-              <option value='ro'>Romanian</option>
-              <option value='ru'>Russian</option>
-              <option value='sk'>Slovak</option>
-              <option value='sl'>Slovenian</option>
-              <option value='es'>Spanish</option>
-              <option value='sw'>Swedish</option>
-              <option value='tr'>Turkish</option>
-              <option value='es'>Ukrainian</option>
-            </select>
-            <div>
-              <textarea
-                title='output'
-                id='translateOutput'
-                readOnly
-                value={outputText}
+              <select
+                title='Language Select'
+                className='langSelect'
+                value={outputLanguage}
+                onChange={event => setOutputLanguage(event.target.value)}
               >
-                {' '}
-              </textarea>
+                <option value='bg'>Bulgarian</option>
+                <option value='zh'>Chinese</option>
+                <option value='da'>Danish</option>
+                <option value='nl'>Dutch</option>
+                <option value='es-us'>English</option>
+                <option value='et'>Estonian</option>
+                <option value='fi'>Finnish</option>
+                <option value='fr'>French</option>
+                <option value='de'>German</option>
+                <option value='el'>Greek</option>
+                <option value='hu'>Hungarian</option>
+                <option value='id'>Indonesian</option>
+                <option value='it'>Italian</option>
+                <option value='ja'>Japanese</option>
+                <option value='lt'>Lithuanian</option>
+                <option value='lv'>Latvian</option>
+                <option value='pl'>Polish</option>
+                <option value='pt-br'>Portuguese(Brazilian)</option>
+                <option value='pt-pt'>Portuguese(Portugal)</option>
+                <option value='ro'>Romanian</option>
+                <option value='ru'>Russian</option>
+                <option value='sk'>Slovak</option>
+                <option value='sl'>Slovenian</option>
+                <option value='es'>Spanish</option>
+                <option value='sw'>Swedish</option>
+                <option value='tr'>Turkish</option>
+                <option value='es'>Ukrainian</option>
+              </select>
+            </div>
+
+            <div id='outputSide' className='boxContainer'>
+              <div>
+                <textarea
+                  title='output'
+                  className='translatorTextAreas'
+                  readOnly
+                  value={outputText}
+                >
+                  {' '}
+                </textarea>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-      <h2 id='translateGuide'>
-        To start a translation click the button between the two boxes
-      </h2>
-    </div>
+        </form>
+      </div>
+    </>
   )
 }
