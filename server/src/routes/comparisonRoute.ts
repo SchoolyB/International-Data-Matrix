44697e555ae11f655207b23b1d73c1c16962606b
Marshall Burns <marshallbcodes@gmail.com>
2023-06-18 13:17:49 -0500

    Add rate limiting to comparison route

--- server/src/routes/comparisonRoute.ts
+++ server/src/routes/comparisonRoute.ts
@@ -1,9 +1,14 @@
 import Comparison from '../models/Comparison'
 import { FastifyInstance } from 'fastify'
+import limit from '@fastify/rate-limit'
 
 export async function comparisonRoute(instance: FastifyInstance) {
   instance.get('/', async (_, reply) => {
     const result = await Comparison.find({})
     return result
+  })
+  await instance.register(limit, {
+    max: 50, //limits each IP to 50 requests per windowMs
+    timeWindow: '1 minute',
   })
 }
