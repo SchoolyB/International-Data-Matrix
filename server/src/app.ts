d8bf2234cc345dbb5c37efc50557c340c5356b5a
Marshall Burns <marshallbcodes@gmail.com>
2023-06-18 15:03:50 -0500

    Changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,55 +5,44 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import { countriesRoute } from './routes/countriesRoute'
 import { translateRoute } from './routes/translateRoute'
-import { comparisonRoute } from './routes/comparisonRoute'
 
 dotenv.config()
 
-// Start server instance one aka country data api
-const serverInstance = fastify({
+const app = fastify({
   logger: false, // set to true for production
 })
 
-const startServerInstance = async () => {
+const start = async () => {
   await mongoose.connect(`${process.env.DATA_BASE}`)
 
-  await serverInstance.register(cors, {
-    origin: 'https://international-data-matrix.vercel', //for production
+  await app.register(cors, {
+    origin: 'https://international-data-matrix.vercel.app', //for production
     // origin: 'http://localhost:5173', //for development
   })
   // Rate limiter
-  await serverInstance.register(limit, {
+  await app.register(limit, {
     max: 50, //limits each IP to 50 requests per windowMs
     timeWindow: '1 minute',
   })
 
-  await serverInstance.register(countriesRoute, {
+  await app.register(countriesRoute, {
     prefix: '/Countries',
   })
 
-  serverInstance.register(comparisonRoute, {
-    prefix: '/Comparison',
-  })
-
-  serverInstance.register(translateRoute, {
+  await app.register(translateRoute, {
     prefix: '/Translator',
   })
 
-  serverInstance.register(limit, {
-    max: 25, //limits each IP to 50 requests per windowMs
-    timeWindow: '1 minute',
-  })
-
   try {
-    const address = await serverInstance.listen(
+    const address = await app.listen(
       { port: 4042, host: '' },
       (err, address) => {
         console.log(`Server listening on ${address}`)
       },
     )
   } catch (e) {
-    serverInstance.log.error(e)
+    app.log.error(e)
   }
 }
-// end of servers
-startServerInstance()
+
+start()
