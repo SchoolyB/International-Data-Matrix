7fd983ec02a37bbfb048a6473799167028505d99
Marshall Burns <marshallbcodes@gmail.com>
2023-06-24 21:12:05 -0500

    Add capital maps/getting google error???

--- client/src/components/CountryWeather.tsx
+++ client/src/components/CountryWeather.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { getWeather } from '../includes/openWeather'
 import { WeatherData } from '../types/weatherData'
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
+import { useMemo } from 'react'
 
 interface Props {
   capital: string
@@ -22,7 +24,6 @@ export default function CountryWeather(props: Props) {
       return
     }
     const data = await getWeather(props.capital)
-
     setWeather({
       main: data.weather.main, //TODO: weather.main not working anymore. used to say 'cloudy, sunny, rainy'
       description: data.weather.id,
@@ -36,27 +37,48 @@ export default function CountryWeather(props: Props) {
     fetchWeather()
   }, [props.capital])
 
+  const mapKey = import.meta.env.VITE_MAP_KEY
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: `${mapKey}` || '',
+  })
+  let mapLat = weather.lat
+  let mapLon = weather.lon
+  const center = useMemo(() => ({ lat: mapLat, lng: mapLon }), [])
+
+  console.log(mapLat, mapLon)
   return (
-    <div className='genInfoLocationAndWeather metaDataSection'>
-      <div className='capital'>
-        <h6>Capital</h6>
-        {props.capital}
+    <>
+      <div className='map' id='smallerMap'>
+        (
+        <GoogleMap
+          mapContainerClassName='map-container'
+          center={center}
+          zoom={7.5}
+        ></GoogleMap>
+        )
       </div>
-      <br />
-      {/* start of location information */}
-      <div className='location'>
-        <h6>Location of Capital</h6>
-        Latitude:{weather.lat} Longitude: {weather.lon}
-      </div>
-      <br />
-      {/* start of weather information */}
-      <div className='weather'>
-        <h6>Current Weather In {props.capital}</h6>
-        
-        Currently {weather.temp}°F
+
+      <div className='genInfoLocationAndWeather metaDataSection'>
+        <div className='capital'>
+          <h6>Capital</h6>
+          {props.capital}
+        </div>
         <br />
-        Feels like {weather.feels_like}°F
+        {/* start of location information */}
+        <div className='location'>
+          <h6>Location of Capital</h6>
+          Latitude:{weather.lat} Longitude: {weather.lon}
+        </div>
+        <br />
+        {/* start of weather information */}
+        <div className='weather'>
+          <h6>Current Weather In {props.capital}</h6>
+          Currently {weather.temp}°F
+          <br />
+          Feels like {weather.feels_like}°F
+        </div>
       </div>
-    </div>
+    </>
   )
 }
